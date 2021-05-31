7476c300f3b276e6c9212a2adf830b3bb215dd43
SooHoonBae <jjajangmeun@gmail.com>
2021-05-31 13:48:47 +0900

    modify better

--- main.js
+++ main.js
@@ -3,17 +3,14 @@ const target = document.querySelector(".target");
 const coordinateNumb = document.querySelector(".coordinate-numb");
 const horisontal = document.querySelector(".horisontal");
 const vertical = document.querySelector(".vertical");
-
-
-window.addEventListener("mousemove",(event)=>{
-    const rect = target.getBoundingClientRect();    
-    const rectH = horisontal.getBoundingClientRect();    
-    const rectV = vertical.getBoundingClientRect();    
-    target.style.left = event.pageX - rect.width/2 + "px";
-    target.style.top = event.pageY - rect.height/2 + "px";
-    coordinateNumb.style.left = event.pageX + 30 + "px";
-    coordinateNumb.style.top = event.pageY + 20 + "px";
-    horisontal.style.top = event.pageY - rectH.height/2 + "px";
-    vertical.style.left = event.pageX - rectV.width/2  + "px";
-    coordinateNumb.innerHTML = `${event.pageX}, ${event.pageY}`;
+document.addEventListener("mousemove",(event)=>{
+    const x = event.clientX;
+    const y = event.clientY;
+    target.style.left =`${x}px`
+    target.style.top =`${y}px`
+    coordinateNumb.style.left =`${x}px` 
+    coordinateNumb.style.top =`${y}px` 
+    vertical.style.left = `${x}px`
+    horisontal.style.top =`${y}px`
+    coordinateNumb.innerHTML = `${x}px, ${y}px`;
  })
