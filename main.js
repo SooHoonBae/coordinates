cb021dc7c1f439a9ea16aa0eae82c84c13a5e902
SooHoonBae <jjajangmeun@gmail.com>
2021-05-31 12:52:44 +0900

    add coodinate number on screen

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 'use strict'
 const target = document.querySelector(".target");
+const coordinateNumb = document.querySelector(".coordinate-numb");
 const horisontal = document.querySelector(".horisontal");
 const vertical = document.querySelector(".vertical");
+
 
 window.addEventListener("mousemove",(event)=>{
     const rect = target.getBoundingClientRect();    
@@ -9,7 +11,9 @@ window.addEventListener("mousemove",(event)=>{
     const rectV = vertical.getBoundingClientRect();    
     target.style.left = event.pageX - rect.width/2 + "px";
     target.style.top = event.pageY - rect.height/2 + "px";
+    coordinateNumb.style.left = event.pageX + 30 + "px";
+    coordinateNumb.style.top = event.pageY + 20 + "px";
     horisontal.style.top = event.pageY - rectH.height/2 + "px";
     vertical.style.left = event.pageX - rectV.width/2  + "px";
-
+    coordinateNumb.innerHTML = `${event.pageX}, ${event.pageY}`;
  })
